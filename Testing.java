cfc4e20aad3fa7be795a809009d99bef04426325
Jacob-Eason <easonj2@wwu.edu>
2021-12-15 19:14:36 -0800

    adding to dev branch

--- Testing.java
+++ Testing.java
@@ -1,7 +1,25 @@
 public class Testing {
 
 
+public Testing(){
+}
+
+
+public static void prepare(int[]array){
+
+    for(int i=0; i<array.length; i++){
+        array[i] = i;
+    }
+
+}
+
     public static void main(String [] args){
+        int[] array = new int[5]; 
+        prepare(array);
+        for(int ele:array){
+            System.out.print(array[ele] +",");
+        }
+        System.out.println();
         System.out.println("lol");
     }
 }
